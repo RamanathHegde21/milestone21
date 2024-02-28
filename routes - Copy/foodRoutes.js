eb7330d6cb6c590cee917efa1916ad0ec85b6156
RamanathHegde21 <ramanathhegde2003@gmail.com>
2024-02-28 16:38:12 +0530

    done

--- routes - Copy/foodRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const foodController = require('../controllers/foodController');
-
-router.get('/', foodController.getAllFood);
-
-module.exports = router;
